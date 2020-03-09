591aa26206d0d0bf2b87ba54429bab9d4ecabca8
Claudia Maganhi <claudia.maganhi@gmail.com>
2020-03-09 16:04:34 -0300

    First commit

---
+++ notes/notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notes
+//
+//  Created by Claudia Cavalini Maganhi on 09/03/20.
+//  Copyright © 2020 Claudia Cavalini Maganhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
