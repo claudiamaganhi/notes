416b8e97ff18713581aae1c90454b95b4653b222
Claudia Maganhi <claudia.maganhi@gmail.com>
2020-03-11 14:32:34 -0300

    Populate tableView and adjust detail screen

--- notes/notes/DetailsViewController.swift
+++ notes/notes/DetailsViewController.swift
@@ -5,10 +5,19 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
     
+    
+    var newNote: ((Note) -> Void)?
+    let date = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setBarButtonItem()
+        dateLabel.text = getDate(date: date)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setTitle()
     }
     
     @IBAction func deleteNote(_ sender: UIButton) {
@@ -19,5 +28,25 @@ class DetailsViewController: UIViewController {
     @IBAction func newNote(_ sender: UIButton) {
     }
     
+    func setBarButtonItem() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(save))
+    }
+    
+    @objc func save() {
+        let note = Note(text: textView.text, date: date)
+        newNote?(note)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func getDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM, yyyy hh:mm"
+        return dateFormatter.string(from: date)
+    }
+    
+    private func setTitle() {
+        navigationItem.largeTitleDisplayMode = .never
+    }
 
 }
--- notes/notes/NotesViewController.swift
+++ notes/notes/NotesViewController.swift
@@ -7,21 +7,76 @@ class NotesViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var notesLabel: UILabel!
     
-
+    var notes = [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        setTitle()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateBottomNotes()
+    }
+    
     @IBAction func newNote(_ sender: UIButton) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: "DetailsViewController") as? DetailsViewController {
+            viewController.newNote = { [weak self] note in
+                self?.notes.append(note)
+                self?.tableView.reloadData()
+            }
+            navigationController?.pushViewController(viewController, animated: true)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           <#code#>
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           <#code#>
-       }
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.selectionStyle = .none
+        
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.text
+        cell.detailTextLabel?.text = getFormattedDate(date: note.date)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewController = storyboard?.instantiateViewController(identifier: "DetailsViewController") as? DetailsViewController {
+            viewController.newNote = { [weak self] note in
+                self?.notes.append(note)
+                tableView.reloadData()
+            }
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+    
+    private func updateBottomNotes() {
+        if notes.isEmpty {
+            notesLabel.text = "0 Notas"
+        } else if notes.count == 1 {
+            notesLabel.text = String(notes.count) + " Nota"
+        } else {
+            notesLabel.text = String(notes.count) + " Notas"
+        }
+    }
+    
+    private func getFormattedDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.string(from: date)
+    }
+    
+    private func setTitle() {
+        title = "Notas"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
     
 }
 
